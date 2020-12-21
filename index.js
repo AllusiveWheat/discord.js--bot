db3b3a5d37ae0e864093802090bf4a26266b279f
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2020-12-20 16:47:38 -0800

    added  index.json

---
+++ index.js
@@ -0,0 +1,18 @@
+// require the discord.js module
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', message => {
+	console.log(message.content);
+});
+
+// login to Discord with your app's token
+client.login('your-token-goes-here');
